92ac82c46d2b8f3f396511117f2c698460c39fb5
Jonathan Rangel <jonathanrangelb@gmail.com>
2019-08-28 22:46:18 -0500

    Eliminando archivos inecesarios para el repo final

--- clase5.js
+++
@@ -1,8 +0,0 @@
-var nombre = 'Jonathan' //si lo declaramos como var, la ventana tendra alcance de esta variable con winows.nombre, por lo tanto podremos llamarla uasndo this.nombre, con nombre no se puede
-
-function imprimirNombreEnMayusculas(nombre) {
-    nombre = nombre.toUpperCase()
-    console.log(nombre + ' ' + this.nombre)
-}
-
-imprimirNombreEnMayusculas(nombre)
